6c04358f2263609fcb53f10b7a38823af16b57c7
Jason Stallings <jason@stallin.gs>
2017-08-22 12:38:44 -0500

    Update for new laylout.

--- app.js
+++ app.js
@@ -4,13 +4,19 @@ $(document).on('ready', function()
 {
 	socket = io("https://viewing-server.herokuapp.com");
 	var $username = $('.username');
-	// Bail if we didn't find a username.
-	if (!$username.length)
+	var $login = $('.my-account');
+
+	// When you're not logged in this element should contain nothing.
+	if (0 === $login.length)
 	{
 		return;
 	}
 	// Build username string. Example: Real Name (username).
-	var username = $('.login a').first().text() + ' (' + $username.text() + ')';
+	var username = $login.find('li').eq(0).text().split(', ')[1];
+	if ($username.length > 0) {
+		 username += ' (' + $username.text() + ')';
+	}
+
 	var page = window.location.pathname;
 	socket.on('connect', function()
 	{
@@ -42,7 +48,7 @@ $(document).on('ready', function()
 
 				$("#viewing-top").text("");
 
-				if (userlist.length == 1)
+				if (userlist.length === 1)
 				{
 					ending = "is also viewing this page.";
 				}
@@ -50,19 +56,18 @@ $(document).on('ready', function()
 				{
 					ending = "are also viewing this page.";
 				}
-				if (flag == 1)
+				if (flag === 1)
 				{
 					if (!$("#viewing-top")[0])
 					{
-						$("#pagebody").css('margin-top', '50px');
-						$("#headline").append('<div id="viewing-top" style="font-size: 14px; color: #fff; line-height: 30px; font-family: Helvetica,sans-serif; background: #45bbe6; border-bottom: 1px solid #dfdfdf; width:100%; height:30px; text-align: center; color: #ffffff">Jason Stallings is also viewing.</div>');
+						// Add after the masthead.
+						$("#masthead").after('<div id="viewing-top" style="font-size: 14px; color: #fff; line-height: 30px; font-family: Helvetica,sans-serif; background: #45bbe6; border-bottom: 1px solid #dfdfdf; width:100%; height:30px; text-align: center; color: #ffffff">Jason Stallings is also viewing.</div>');
 					}
 					$("#viewing-top").text(userlist.join(", ") + " " + ending);
 				}
 			}
 			else
 			{
-				$("#pagebody").css('margin-top', '29px');
 				$("#viewing-top").remove();
 			}
 			users = data;
